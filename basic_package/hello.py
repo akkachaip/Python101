a83f1cbe55a620197afcef8bb3c7c0db16779556
akkachaip <32292289+akkachaip@users.noreply.github.com>
2023-02-22 21:58:13 +0700

    แก้ไขHelloและ upload folder basic package

---
+++ basic_package/hello.py
@@ -0,0 +1,8 @@
+print ('Hello Uncle')
+print ('Hello Ake')
+print ('Hello Tatum')
+
+friend = ['Loong','Pa Lek','Loong Dam']
+
+print(friend[0])
+
---
+++ basic_package/info.py
@@ -0,0 +1,2 @@
+name='Akkachai'
+lastname = 'Pattanadilok'
